b8763c95d35707486308dd0956527d3f9f5afe31
deq <shrsingh@deqode.com>
2022-03-09 18:21:18 +0530

    jwt authentication

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,23 +1,30 @@
 import jwt from 'jsonwebtoken';
 require('dotenv').config();
 
-export async function authentication(req, res, next) {
+export async function authentication(req, res) {
+  let jwtSecretKey = process.env.TOKEN_KEY;
+  let data = {
+    time: Date(),
+  };
+  const token = jwt.sign(data, jwtSecretKey);
+  console.log(token)
+  res.send(token);
+};
+
+
+export async function verifyToken(req, res, next) {
   const authHeader = req.headers['authorization'];
   const token = authHeader.split(" ")[1];
   if (!token) {
     return res.status(403).send("A token is required for authentication");
   }
-
   try {
-    const decoded = jwt.sign(token, process.env.TOKEN_KEY)
-    console.log(decoded)
+    const decoded = jwt.verify(token, process.env.TOKEN_KEY)
     req.user = decoded;
   } catch (err) {
     console.log(err)
     return res.status(401).send("Invalid Token");
   }
-
   return next();
-
 };
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,12 @@
 import express from 'express';
-// import { authentication, requestTime } from '../middleware/authentication';
+import { authentication, verifyToken } from '../middleware/authentication';
 import controller from '../controllers/auth.controller';
 
 const router = express.Router();
 
-router.post('/auth/setdata', controller.setfiledata);
-router.get('/auth/getdata', controller.getfiledata);
-router.post('/auth/upload', controller.uploadFile);
+router.post('/auth/setdata', verifyToken, controller.setfiledata);
+router.get('/auth/getdata', verifyToken, controller.getfiledata);
+router.post('/auth/upload', verifyToken, controller.uploadFile);
+router.post('/auth/generateToken', authentication);
 
 export default router;
